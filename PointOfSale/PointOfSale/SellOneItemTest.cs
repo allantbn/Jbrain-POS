71be8f3c8ab7a61b2eb4115d0bf8198e8598411c
allantbn <allan.tan@titansoft.com.sg>
2016-10-20 15:07:08 +0800

    Single responsibilty code

--- PointOfSale/PointOfSale/SellOneItemTest.cs
+++ PointOfSale/PointOfSale/SellOneItemTest.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -9,97 +8,95 @@ namespace PointOfSale
     [TestClass]
     public class SellOneItemTest
     {
-        private Display display;
-        private Sale sale;
+        private Display _display;
+        private Sale _sale;
 
         [TestInitialize]
         public void TestInitialize()
         {
-            display = new Display();
-            sale = new Sale(display, new Dictionary<String, String>()
-            {
-                {"12345", "$7.95"},
-                {"23456", "$12.50"}
-            });
+            _display = new Display();
+            _sale = new Sale(_display, new Catalog(new Dictionary<string, string>()
+                    {
+                        { "12345", "$7.95"},
+                        {"23456", "$12.50"}
+                    }
+                    )
+                );
         }
 
         [TestMethod]
         public void ProductFound()
         {
-            sale.OnBarcode("12345");
-            Assert.AreEqual("$7.95", display.GetText());
+            _sale.OnBarcode("12345");
+            Assert.AreEqual("$7.95", _display.GetText());
         }
 
         [TestMethod]
         public void AnotherProductFound()
         {
-            sale.OnBarcode("23456");
-            Assert.AreEqual("$12.50", display.GetText());
+            _sale.OnBarcode("23456");
+            Assert.AreEqual("$12.50", _display.GetText());
         }
 
         [TestMethod]
         public void ProductNotFound()
         {
-            sale.OnBarcode("99999");
-            Assert.AreEqual("Product not found for 99999", display.GetText());
+            _sale.OnBarcode("99999");
+            Assert.AreEqual("Product not found for 99999", _display.GetText());
         }
 
         [TestMethod]
         public void EmtpyBarcode()
         {
-            Sale sale = new Sale(display, null);
+            var sale = new Sale(_display, new Catalog(null));
             sale.OnBarcode("");
-            Assert.AreEqual("Scanning error: empty barcode", display.GetText());
+            Assert.AreEqual("Scanning error: empty barcode", _display.GetText());
         }
     }
-  
+
     internal class Sale
     {
         private readonly Display _display;
-        private readonly Dictionary<string, string> _pricesByBarcode;
+        private readonly Catalog _catalog;
 
-        public Sale(Display dislpay, Dictionary<string, string> pricesByBarcode)
+        public Sale(Display dislpay, Catalog catalog)
         {
+            _catalog = catalog;
             this._display = dislpay;
-            this._pricesByBarcode = pricesByBarcode;
         }
 
         public void OnBarcode(string barcode)
         {
             if ("".Equals(barcode))
             {
-                DisplayEmptyBarCodeMessage();
+                _display.DisplayEmptyBarCodeMessage();
                 return;
             }
 
-            if (_pricesByBarcode.ContainsKey(barcode))
+            string priceAsText = _catalog.FindPrice(barcode);
+            if (priceAsText == null)
             {
-                DisplayePrice(FindPrice(barcode));
+                _display.DisplayProductNotFoundMessage(barcode);
             }
             else
             {
-                DisplayProductNotFoundMessage(barcode);
+                _display.DisplayePrice(priceAsText);
             }
         }
+    }
 
-        private void DisplayProductNotFoundMessage(string barcode)
+    internal class Catalog
+    {
+        private readonly Dictionary<string, string> _pricesByBarcode;
+
+        public Catalog(Dictionary<string, string> pricesByBarcode)
         {
-            this._display.SetText("Product not found for " + barcode);
+            _pricesByBarcode = pricesByBarcode;
         }
 
-        private void DisplayePrice(string priceAsText)
+        public string FindPrice(string barcode)
         {
-            this._display.SetText(priceAsText);
-        }
-
-        private string FindPrice(string barcode)
-        {
-            return _pricesByBarcode[barcode];
-        }
-
-        private void DisplayEmptyBarCodeMessage()
-        {
-            this._display.SetText("Scanning error: empty barcode");
+            return _pricesByBarcode.ContainsKey(barcode) ? _pricesByBarcode[barcode] : null;
         }
     }
 
@@ -112,9 +109,19 @@ namespace PointOfSale
             return _text;
         }
 
-        public void SetText(string text)
+        public void DisplayePrice(string priceAsText)
         {
-            this._text = text;
+            this._text = priceAsText;
+        }
+
+        public void DisplayProductNotFoundMessage(string barcode)
+        {
+            this._text = "Product not found for " + barcode;
+        }
+
+        public void DisplayEmptyBarCodeMessage()
+        {
+            this._text = "Scanning error: empty barcode";
         }
     }
 }
